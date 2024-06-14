a3befd534fd47d9423802ffa6c8fd83c3f92a13b
Nicks <nikhilpatil300@gmail.com>
2024-06-14 11:01:56 +0530

    window or document is not part of node

--- index.js
+++ index.js
@@ -9,4 +9,14 @@ function sayHello(name) {
 }
 
 
-sayHello('Mosh')
+// sayHello('Mosh')
+
+
+console.log(window)
+
+//  we got exception , windoow is not defined 
+//  in node we don't have window or document object , this are part
+// runtime entironment that we get with browser
+
+//  in node we have other object that we are working with files  , operating 
+// system and networkds and so on
